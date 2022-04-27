81f1dc303224889e445016c165a8ffa08992961d
andyarensman <86622025+andyarensman@users.noreply.github.com>
2022-04-27 12:49:07 -0700

    lesson 4 - tracking guesses

--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -23,8 +23,20 @@ const useWordle = (solution) => {
 
   // ahndle keyip event & track current guess
   // if the user presses enter, add the new guess
-  const handleKeyup = () => {
-
+  const handleKeyup = ({ key }) => {
+    if ( key === 'Backspace') {
+      setCurrentGuess((prev) => {
+        return prev.slice(0, -1)
+      })
+      return
+    }
+    if (/^[A-Za-z]$/.test(key)) {
+      if ( currentGuess.length < 5) {
+        setCurrentGuess((prev) => {
+          return prev + key
+        })
+      }
+    }
   }
 
   return {turn, currentGuess, guesses, isCorrect, handleKeyup}
